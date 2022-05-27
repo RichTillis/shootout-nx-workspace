238cf86089994074fba31eec130ce04416e32c3a
Rich Tillis <rich@richtillis.com>
2022-05-26 18:30:06 -0700

    initial commit

---
+++ apps/shootout-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shootout-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shootout-web';
+}
